42258b782c2e6d8cd65e03bc208760a3544ccd1b
Harry Newton <harryjnewton98@gmail.com>
2022-06-08 14:42:22 +1000

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Library } from "./components/Library";
+import { LocalLibrary } from "@material-ui/icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="app">
+      <div className="header">
+        <LocalLibrary />
+        <h1>Library</h1>
+      </div>
+      <div className="library">
+        <Library />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
